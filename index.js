43a2906e157cb06b04d08cd68ee65bce765a7356
Shehab Chowdhury <shehabc58@gmail.com>
2023-09-20 16:32:10 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = [`Milo`, `Otis`, `Garfield`]
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+function appendCat(name) {
+    let backCats = [...cats]
+    backCats.push(name)
+    return(backCats)
+}
+function prependCat(name) {
+    let copyCats = [...cats]
+    copyCats.unshift(name)
+    return(copyCats)
+}
+function removeLastCat() {
+    let lastCat = [...cats]
+    lastCat.pop()
+    return lastCat
+}
+function removeFirstCat() {
+    let firstCat = [...cats]
+    firstCat.shift()
+    return firstCat
+}
